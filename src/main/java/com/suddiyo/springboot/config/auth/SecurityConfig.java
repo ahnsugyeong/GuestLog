0962f1273f9dd6d4aa3e3f46355c4497361c3a03
suddiyo <suddii01@sju.ac.kr>
2023-06-29 15:52:27 +0900

    profile API 추가

--- src/main/java/com/suddiyo/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/suddiyo/springboot/config/auth/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                 .headers().frameOptions().disable() // HTML 삽입 취약점 방어로, iframe, object 등에 삽입해서 제어하거나 클릭하는 공격을 방지하는 옵션 삭제. spring 통해서 h2 console 확인할 때 사용
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
---
+++ src/test/java/com/suddiyo/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,30 @@
+package com.suddiyo.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ProfileControllerTest {
+    @LocalServerPort private int port;
+    @Autowired private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception {
+        String expected = "default";
+
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+
+}
