0084d343e64d5186b590e4913de32f2dec620f45
suddiyo <suddii01@sju.ac.kr>
2023-07-02 01:09:40 +0900

    도메인 등록 및 View & 기능 보완

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -10,7 +10,7 @@ function find_idle_profile() {
   then
     CURRENT_PROFILE=real2
   else
-    CURRENT_PROFILE=$(curl -s https://suddiyo-springboot-webservice.site/profile)
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
   if [ ${CURRENT_PROFILE} == real1 ]
